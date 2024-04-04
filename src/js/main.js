3e594ccef30fec9f13aaa16de385d13d1e20e23a
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-04 12:52:20 +0200

    Lagt till hänelselyssnare för submitknappen i initfunktionen. Kontrollerar formulärets värden och kallar på funktion för att skapa ny erfarenhet.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
 // Importerar modul
 import { fetchExperiences } from './experiences.js';
+import { createExperience } from './add.js';
 
 // Hämtar element och lagrar i variabler
 const menuToggle = document.querySelector(".menu-toggle");
@@ -9,6 +10,7 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const menuIcon = document.querySelector(".fa-bars");
 const closeIcon = document.querySelector(".fa-xmark");
 const containerEl = document.getElementById("overlay");
+const submitBtn = document.getElementById("submit");
 
 // Skapar klickhändelselyssnare för menyknappen, anonym funktion
 menuToggle.addEventListener("click", () => {
@@ -33,4 +35,25 @@ menuToggle.addEventListener("click", () => {
 window.onload = init;
 function init() {
     fetchExperiences(); // Anropar funktion för att hämta arbetserfarenheter
+
+    // Kontrollerar om submit-knappen finns på sidan
+    if (submitBtn) {
+
+        // Skapar isåfall en händelselyssnare vid klick
+        submitBtn.addEventListener("click", (event) => {
+            event.preventDefault(); // Förhindrar formulärets standardbeteende (så att sidan inte laddas om)
+
+            // Hämtar värdena från formuläret
+            const companyname = document.getElementById('companyname').value;
+            const jobtitle = document.getElementById('jobtitle').value;
+            const location = document.getElementById('location').value;
+            const startdate = document.getElementById('startdate').value;
+            const enddate = document.getElementById('enddate').value;
+            const description = document.getElementById('description').value;
+
+            // Anropar funktion för att skapa ny jobberfarenhet, skickar med värdena från formuläret
+            createExperience(companyname, jobtitle, location, startdate, enddate, description);
+
+        });
+    }
 }
